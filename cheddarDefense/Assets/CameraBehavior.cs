ace207d6ad44fa37f8b5d8a62c7b3d4a2af11e91
LukeDuhe <Luke7490@gmail.com>
2018-03-19 15:26:56 -0500

    Basic player movement and collisions work

---
+++ cheddarDefense/Assets/CameraBehavior.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBehavior : MonoBehaviour {
+
+	public GameObject player;       //Public variable to store a reference to the player game object
+
+	public Vector3 offset;   //Private variable to store the offset distance between the player and camera
+
+	// LateUpdate is called after Update each frame
+
+	void Start(){
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+
+	void LateUpdate () 
+	{
+		// Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
+		transform.position = player.transform.position + offset;
+	}
+}
