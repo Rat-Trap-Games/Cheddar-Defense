d0afb0c008ce3231f97df2d6a7cc633463b00e60
Sam Miller <sithpow@gmail.com>
2018-04-21 18:51:48 -0500

    change to CHeddar Defense v3

---
+++ Cheddar Defense Version 3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movementSpeed;
+    public float moveHorizontal;
+    public float moveVertical;
+
+    void Update()
+    {
+        moveHorizontal = Input.GetAxisRaw ("PlayerHorizontal");
+        moveVertical = Input.GetAxisRaw ("PlayerVertical");
+  
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        if(moveHorizontal != 0 || moveVertical != 0){
+            transform.rotation = Quaternion.LookRotation(movement);
+        }
+  
+        transform.Translate (movement * movementSpeed * Time.deltaTime, Space.World);
+    }
+
+    void OnCollisionEnter(Collision col) { 
+        Debug.Log(col.gameObject.tag);
+    }
+}
